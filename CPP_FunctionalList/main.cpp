a936240c6a43aa4e704f7616c87a0e6c06ca861f
Ferenc Szalma <szalmaf@gmail.com>
2017-01-14 18:01:41 -0500

    Initial Commit

---
+++ CPP_FunctionalList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP_FunctionalList
+//
+//  Created by Ferenc Szalma on 1/14/17.
+//  Copyright © 2017 Ferenc Szalma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
