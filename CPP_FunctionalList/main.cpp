32990bd04c7857bd9c6f3821714a385b0dcf8c4e
Ferenc Szalma <szalmaf@gmail.com>
2017-01-18 13:54:58 -0500

    Add strToList

--- CPP_FunctionalList/main.cpp
+++ CPP_FunctionalList/main.cpp
@@ -13,10 +13,12 @@ template<class T>
 class List {
     struct Item
     {
-        Item(T v, std::shared_ptr<const Item> const & tail) : _val(v), _next(tail) {}
+        Item(T v, std::shared_ptr<const Item> const & tail)
+            : _val(v), _next(std::move(tail)) {}
         T _val;
         std::shared_ptr<const Item> _next;
     };
+    friend Item;
 public:
     List() {} // empty list constructor
     List(T v, List const & tail) : _head(std::make_shared<Item>(v, tail._head)) {} // x:xs recursive constructor; tail won't change ever, it's persistent
@@ -34,7 +36,8 @@ public:
         return List(_head->_next);
     }
     
-    List push_front(T v) const { //returns a new list with v as head, tail is same as the original list
+    List push_front(T v) const
+    { //returns a new list with v as head, tail is same as the original list
         return List(v, *this);
     }
     
@@ -54,9 +57,20 @@ List<U> fmap(F f, List<T> lst)
         return List<U>(f(lst.front()), fmap<U>(f, lst.pop_front()));
 }
 
+List<char> strToList(const std::string &str)
+{
+    auto l = List<char>();
+    for_each(str.rbegin(), str.rend(), [&l](char c){ l = l.push_front(c); });
+    return l;
+}
 
 int main(int argc, const char * argv[]) {
-
+    
+    List<char> charLst = strToList("abdUIjk");
+    
+//    auto len = strlen(charLst)-1;
+//    auto charLst1 = std::for_each(charLst, &charLst[len], toupper);
+//    auto charLst2 = fmap<char>(toupper, charLst);
     
     return 0;
 }
